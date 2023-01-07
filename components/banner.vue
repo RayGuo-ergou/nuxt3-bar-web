77dd731c03877cfa87cba84211fdd83b98ac18bb
Ray Guo <ma2808203259@hotmail.com>
2023-01-07 16:03:35 +1100

    feat: banner for events page

---
+++ components/banner.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import { PropType } from 'vue'
+
+type Button = {
+  text: string
+  color: string
+  variant: string
+  onClick: Function
+}
+
+defineProps({
+  buttons: {
+    type: Array as PropType<Button[]>,
+    default: () => [
+      {
+        text: 'Button',
+        color: 'primary',
+        variant: 'solid',
+        onClick: () => {},
+      },
+    ],
+  },
+
+  title: {
+    type: String,
+    default: 'Title',
+  },
+
+  subtitle: {
+    type: String,
+    default: 'Subtitle',
+  },
+
+  image: {
+    type: String,
+    // random image from unsplash
+    default:
+      'https://images.unsplash.com/photo-1575444758702-4a6b9222336e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+  },
+
+  description: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <section
+    class="relative bg-cover bg-center bg-no-repeat"
+    :class="`bg-[url(${image})]`"
+  >
+    <div
+      class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"
+    ></div>
+
+    <div
+      class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+    >
+      <div class="max-w-xl text-center sm:text-left">
+        <h1 class="text-3xl font-extrabold sm:text-5xl text-gray-700">
+          {{ title }}
+          <strong class="block font-extrabold text-primary-700">
+            {{ subtitle }}
+          </strong>
+        </h1>
+
+        <p class="mt-4 text-gray-800 max-w-lg sm:text-xl sm:leading-relaxed">
+          {{ description }}
+        </p>
+
+        <div class="mt-8 flex flex-wrap gap-4 text-center">
+          <div v-for="button in buttons" :key="button.text">
+            <a
+              :color="button.color"
+              :variant="button.variant"
+              class="block w-full rounded px-12 py-3 text-sm font-medium text-white shadow focus:outline-none sm:w-auto"
+              :class="`bg-${button.color}-${
+                button.variant === 'solid' ? '600' : '100'
+              } hover:bg-${button.color}-${
+                button.variant === 'solid' ? '700' : '200'
+              } focus:ring-${button.color}-${
+                button.variant === 'solid' ? '500' : '200'
+              } active:bg${button.color}-${
+                button.variant === 'solid' ? '500' : '100'
+              }}`"
+              @click="button.onClick"
+            >
+              {{ button.text }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,97 +1,146 @@
 <script lang="ts" setup>
+const showList = ref(false)
+// create a ref called tab, the value can only be 'new' or 'old' or undefined
+const tab = ref<'new' | 'old' | undefined>(undefined)
+
 // compiler macro
 definePageMeta({
   layout: 'page',
 })
+
+const buttons = [
+  {
+    text: 'Upcoming events',
+    color: 'primary',
+    variant: 'solid',
+    onClick: async () => {
+      showList.value = true
+      tab.value = 'new'
+      await nextTick()
+      const e = document.getElementById('newEvent')
+      if (e) {
+        e.scrollIntoView({ behavior: 'smooth' })
+      }
+    },
+  },
+  {
+    text: 'Past events',
+    color: 'primary',
+    variant: 'solid',
+    onClick: async () => {
+      showList.value = true
+      tab.value = 'old'
+      await nextTick()
+      const e = document.getElementById('oldEvent')
+      if (e) {
+        e.scrollIntoView({ behavior: 'smooth' })
+      }
+    },
+  },
+]
 </script>
 
 <template>
-  <div class="mx-auto max-w-7xl w-full h-full">
-    <PageWrapper>
-      <PageHeader>
-        <PageTitle text="New events" class="capitalize" />
-      </PageHeader>
-      <PageBody>
-        <ContentList v-slot="{ list }" path="/events/new">
-          <PageSection v-for="article in list" :key="article._path">
-            <div
-              class="block hover:no-underline p-6 space-x-6 rounded border border-gray-900/10 dark:border-gray-50/[0.2]"
-            >
-              <div class="mt-1 text-slate-600 dark:text-gray-400 text-right">
-                <div>{{ article.date }}</div>
-                <Anchor
-                  class="text-sm flex items-center justify-end space-x-1"
-                  :href="`https://www.github.com/${article.author}`"
-                >
-                  <icon-mdi:github-face class="text-xs" />
-                  <span>{{ article.author }}</span>
-                </Anchor>
-              </div>
-              <div class="flex flex-col">
+  <div>
+    <Banner :buttons="buttons" />
+    <div
+      class="mx-auto max-w-7xl w-full h-full"
+      :class="[showList ? 'min-h-[500px]' : '']"
+    >
+      <PageWrapper>
+        <div v-if="showList && tab === 'new'" id="newEvent">
+          <PageHeader>
+            <PageTitle text="New events" class="capitalize" />
+          </PageHeader>
+          <PageBody>
+            <ContentList v-slot="{ list }" path="/events/new">
+              <PageSection v-for="article in list" :key="article._path">
                 <div
-                  class="text-xl font-semibold text-slate-800 dark:text-gray-50"
+                  class="block hover:no-underline p-6 space-x-6 rounded border border-gray-900/10 dark:border-gray-50/[0.2]"
                 >
-                  {{ article.title }}
-                </div>
-                <div class="text-slate-700 dark:text-gray-300 mb-1">
-                  {{ article.description }}
-                </div>
-                <div class="flex">
-                  <Anchor
-                    class="text-sm flex space-x-1 items-center text-primary-500"
-                    :to="article._path"
+                  <div
+                    class="mt-1 text-slate-600 dark:text-gray-400 text-right"
                   >
-                    <span>{{ $t('others.learn_more') }}</span>
-                    <icon:ic:baseline-arrow-right-alt class="text-sm" />
-                  </Anchor>
+                    <div>{{ article.date }}</div>
+                    <Anchor
+                      class="text-sm flex items-center justify-end space-x-1"
+                      :href="`https://www.github.com/${article.author}`"
+                    >
+                      <icon-mdi:github-face class="text-xs" />
+                      <span>{{ article.author }}</span>
+                    </Anchor>
+                  </div>
+                  <div class="flex flex-col">
+                    <div
+                      class="text-xl font-semibold text-slate-800 dark:text-gray-50"
+                    >
+                      {{ article.title }}
+                    </div>
+                    <div class="text-slate-700 dark:text-gray-300 mb-1">
+                      {{ article.description }}
+                    </div>
+                    <div class="flex">
+                      <Anchor
+                        class="text-sm flex space-x-1 items-center text-primary-500"
+                        :to="article._path"
+                      >
+                        <span>{{ $t('others.learn_more') }}</span>
+                        <icon:ic:baseline-arrow-right-alt class="text-sm" />
+                      </Anchor>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </PageSection>
-        </ContentList>
-      </PageBody>
-      <PageHeader>
-        <PageTitle text="old events" class="capitalize" />
-      </PageHeader>
-      <PageBody>
-        <ContentList v-slot="{ list }" path="/events/old">
-          <PageSection v-for="article in list" :key="article._path">
-            <div
-              class="block hover:no-underline p-6 space-x-6 rounded border border-gray-900/10 dark:border-gray-50/[0.2]"
-            >
-              <div class="mt-1 text-slate-600 dark:text-gray-400 text-right">
-                <div>{{ article.date }}</div>
-                <Anchor
-                  class="text-sm flex items-center justify-end space-x-1"
-                  :href="`https://www.github.com/${article.author}`"
-                >
-                  <icon-mdi:github-face class="text-xs" />
-                  <span>{{ article.author }}</span>
-                </Anchor>
-              </div>
-              <div class="flex flex-col">
+              </PageSection>
+            </ContentList>
+          </PageBody>
+        </div>
+        <div v-if="showList && tab === 'old'" id="oldEvent">
+          <PageHeader>
+            <PageTitle text="old events" class="capitalize" />
+          </PageHeader>
+          <PageBody>
+            <ContentList v-slot="{ list }" path="/events/old">
+              <PageSection v-for="article in list" :key="article._path">
                 <div
-                  class="text-xl font-semibold text-slate-800 dark:text-gray-50"
+                  class="block hover:no-underline p-6 space-x-6 rounded border border-gray-900/10 dark:border-gray-50/[0.2]"
                 >
-                  {{ article.title }}
-                </div>
-                <div class="text-slate-700 dark:text-gray-300 mb-1">
-                  {{ article.description }}
-                </div>
-                <div class="flex">
-                  <Anchor
-                    class="text-sm flex space-x-1 items-center text-primary-500"
-                    :to="article._path"
+                  <div
+                    class="mt-1 text-slate-600 dark:text-gray-400 text-right"
                   >
-                    <span>{{ $t('others.learn_more') }}</span>
-                    <icon:ic:baseline-arrow-right-alt class="text-sm" />
-                  </Anchor>
+                    <div>{{ article.date }}</div>
+                    <Anchor
+                      class="text-sm flex items-center justify-end space-x-1"
+                      :href="`https://www.github.com/${article.author}`"
+                    >
+                      <icon-mdi:github-face class="text-xs" />
+                      <span>{{ article.author }}</span>
+                    </Anchor>
+                  </div>
+                  <div class="flex flex-col">
+                    <div
+                      class="text-xl font-semibold text-slate-800 dark:text-gray-50"
+                    >
+                      {{ article.title }}
+                    </div>
+                    <div class="text-slate-700 dark:text-gray-300 mb-1">
+                      {{ article.description }}
+                    </div>
+                    <div class="flex">
+                      <Anchor
+                        class="text-sm flex space-x-1 items-center text-primary-500"
+                        :to="article._path"
+                      >
+                        <span>{{ $t('others.learn_more') }}</span>
+                        <icon:ic:baseline-arrow-right-alt class="text-sm" />
+                      </Anchor>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </PageSection>
-        </ContentList>
-      </PageBody>
-    </PageWrapper>
+              </PageSection>
+            </ContentList>
+          </PageBody>
+        </div>
+      </PageWrapper>
+    </div>
   </div>
 </template>
