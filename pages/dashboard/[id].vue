290c59c123c8567217374934e8b1acca04d5a192
Ray Guo <ma2808203259@hotmail.com>
2023-02-11 01:44:17 +1100

    feat: init dashboard

---
+++ pages/dashboard/[id].vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const id = useRoute().params.id
+// compiler macro
+definePageMeta({
+  layout: 'page',
+})
+useHead(() => ({
+  title: `Dashboard - ${id}`,
+}))
+</script>
+
+<template>
+  <PageWrapper>
+    <PageHeader>
+      <PageTitle text="" class="capitalize" />
+    </PageHeader>
+    <PageBody>
+      <PageSection>
+        <div>{{ id }}</div>
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
