94295536fc89621b4c6eb9aab0f90c8cff2e556a
Ray Guo <ma2808203259@hotmail.com>
2023-02-16 18:10:58 +1100

    fix: workaround for plugin warning

--- plugins/toastification.ts
+++ plugins/toastification.ts
@@ -1,5 +1,10 @@
-import Toast from 'vue-toastification'
-// Import the CSS or use your own!
+/**
+ * XXX: This is a workaround for
+ * A plugin must either be a function or an object with an "install" function.
+ * @link https://github.com/Maronato/vue-toastification/issues/341
+ */
+// @ts-ignore
+import Toast from 'vue-toastification/dist/index.mjs'
 import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
