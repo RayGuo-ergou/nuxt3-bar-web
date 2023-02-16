c622dbbd10296753783fc3e6081cf4d881cdc2c9
Ray Guo <ma2808203259@hotmail.com>
2023-02-16 17:56:11 +1100

    fix: fix the title height and trpc error handling database error

--- composables/useTRPCError.ts
+++ composables/useTRPCError.ts
@@ -1,12 +1,16 @@
 import { TRPCClientError } from '@trpc/client'
 
-export const useTRPCError = (e: any) => {
-  if (e instanceof TRPCClientError) {
-    const errors: { message: string; path: string[] }[] | null = JSON.parse(
-      e.message
-    )
-    if (!errors) return 'Something went wrong'
-    return errors[0].message ? errors[0].message : 'Something went wrong'
+export const useTRPCError = (TRPCError: any) => {
+  if (TRPCError instanceof TRPCClientError) {
+    try {
+      const errors: { message: string; path: string[] }[] | null = JSON.parse(
+        TRPCError.message
+      )
+      if (!errors) return 'Something went wrong'
+      return errors[0].message ? errors[0].message : 'Something went wrong'
+    } catch (e: any) {
+      return TRPCError.message ? TRPCError.message : 'Something went wrong'
+    }
   }
   return 'Something went wrong'
 }
